9ce80b7c6bf53fe44d6f933aa68996322c69ed0f
apoorvsingh13 <apoorv.singh@pepsales.ai>
2024-11-15 22:45:20 +0530

    All kinds of for loops but basic.

--- rust-prog-conditionals-loops/src/main.rs
+++ rust-prog-conditionals-loops/src/main.rs
@@ -11,4 +11,36 @@ fn main() {
     } else {
         println!("I don't know what is it.");
     }
+
+    // For loop with range.
+    // Basic for loop
+
+    for _ in 0..10 {
+        println!("This is the just to print this statement 10 times.");
+    }
+
+    for i in 0..10 {
+        println!("Current value of i is : {}", i);
+    }
+
+    let sentence: String = String::from("Apoorv");
+    let first_word: String = get_first_word(sentence);
+
+    println!("The first word is {}", first_word);
+}
+
+fn get_first_word(sentence: String) -> String {
+    let mut ans: String = String::new();
+
+    //for loop for iterating over iterables here String, but we can also do if for array, heap and
+    //other iterables.
+    for char in sentence.chars() {
+        ans.push_str(char.to_string().as_str());
+
+        if char == ' ' {
+            break;
+        }
+    }
+
+    return ans;
 }
