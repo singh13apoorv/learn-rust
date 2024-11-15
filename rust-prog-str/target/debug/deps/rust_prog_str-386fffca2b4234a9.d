8ee1310867a780b3864a8ab0393efa9529f0a05b
apoorvsingh13 <apoorv.singh@pepsales.ai>
2024-11-15 20:15:26 +0530

    rust program initiation using cargo init.

---
+++ rust-prog-str/target/debug/deps/rust_prog_str-386fffca2b4234a9.d
@@ -0,0 +1,5 @@
+/Users/apoorvsingh/Developer/study/rust/rust-prog-str/target/debug/deps/librust_prog_str-386fffca2b4234a9.rmeta: src/main.rs
+
+/Users/apoorvsingh/Developer/study/rust/rust-prog-str/target/debug/deps/rust_prog_str-386fffca2b4234a9.d: src/main.rs
+
+src/main.rs:
---
+++ rust-prog-str/target/debug/deps/rust_prog_str-a3679c6b99f2937f.d
@@ -0,0 +1,5 @@
+/Users/apoorvsingh/Developer/study/rust/rust-prog-str/target/debug/deps/librust_prog_str-a3679c6b99f2937f.rmeta: src/main.rs
+
+/Users/apoorvsingh/Developer/study/rust/rust-prog-str/target/debug/deps/rust_prog_str-a3679c6b99f2937f.d: src/main.rs
+
+src/main.rs:
